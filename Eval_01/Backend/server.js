036cfc9a954eccc941d4607e738cb3d65bacdda2
soumyadeepdutta7 <soumyadeepdutta.sd@gmail.com>
2024-08-03 17:09:49 +0530

    Updated changes

--- Eval_01/Backend/server.js
+++ Eval_01/Backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const connectDB = require('./config/db');
 const config = require('./config/config');
 const cors = require('cors');
 
-const app = express();
 
+const app = express();
 
 connectDB();
 
@@ -18,6 +20,14 @@ app.use('/api/countries', require('./routes/countryRoutes'));
 app.use('/api/favorites', require('./routes/favoriteRoutes'));
 app.use('/api/history', require('./routes/historyRoutes'));
 
-const PORT = config.port || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const options = {
+  key: fs.readFileSync('path/to/your/key.pem'), 
+  cert: fs.readFileSync('path/to/your/cert.pem') 
+};
+
+
+const PORT = config.port || 5000;
+https.createServer(options, app).listen(PORT, () => {
+  console.log(`Server started on https://localhost:${PORT}`);
+});
